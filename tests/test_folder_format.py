33d11508bc36c2070944f0e3d29fd8e0aa275f43
Camille <78221213+clatapie@users.noreply.github.com>
2023-04-28 18:47:54 +0200

    Adding folder_fornat unit tests

---
+++ tests/test_folder_format.py
@@ -0,0 +1,40 @@
+import os
+
+import ansys.dita.ast.folder_format as ff
+import pytest
+
+# @pytest.fixture
+# def folder_path(ghdir):
+#     folder_path = os.path.join(ghdir, "mapdl-cmd-doc")
+#     return folder_path
+
+# ##########################################################+
+# ##########################################################
+# # To run the Unit Tests locally
+# ##########################################################
+# ##########################################################
+
+
+@pytest.fixture
+def folder_path():
+    return "D:/repos/pyansys/mapdl-cmd-doc-generalized"
+
+
+# ##########################################################
+# ##########################################################
+
+
+def test_xml_path(folder_path):
+    assert folder_path == ff.xml_path(folder_path)
+
+
+def test_missing_xml_path():
+    with pytest.raises(FileNotFoundError, match="Documentation path at  does not exist"):
+        ff.xml_path()
+
+
+def test_get_path(folder_path):
+    assert os.path.join(folder_path, "graphics") == ff.get_paths(folder_path)[0]
+    assert os.path.join(folder_path, "links") == ff.get_paths(folder_path)[1]
+    assert os.path.join(folder_path, "terms") == ff.get_paths(folder_path)[2]
+    assert os.path.join(folder_path, "xml") == ff.get_paths(folder_path)[3]
