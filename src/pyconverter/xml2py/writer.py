768ff72cc89d2aacc44707a4cabe47faacac3be6
Camille <78221213+clatapie@users.noreply.github.com>
2024-07-30 17:08:16 +0200

    Update src/pyconverter/xml2py/writer.py
    Co-authored-by: German <28149841+germa89@users.noreply.github.com>

--- src/pyconverter/xml2py/writer.py
+++ src/pyconverter/xml2py/writer.py
@@ -441,8 +441,6 @@ def write_source(
                 initial_class_name = specific_classes[initial_class_name]
             class_name = initial_class_name.title().replace(" ", "").replace("/", "")
             file_name = initial_class_name.replace(" ", "_").replace("/", "_").lower()
-            if file_name == "":
-                pass
 
             file_path = os.path.join(module_path, f"{file_name}.py")
             if not os.path.isfile(file_path):
