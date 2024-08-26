58b16590b0c7c13dd9bb55bcceb87570fb843500
Camille <78221213+clatapie@users.noreply.github.com>
2024-08-26 15:43:48 +0200

    fix: ``make_package_doc.ps1``

--- make_package_doc.ps1
+++ make_package_doc.ps1
@@ -1,9 +1,20 @@
 #! /bin/sh
 
+function Green
+{
+    process { Write-Host $_ -ForegroundColor Green }
+}
+
 deactivate
 cd .\package\
 python -m venv .venv
 .\.venv\Scripts\activate
+ Write-Output "A new virtual environment has been created within the package folder." | Green
 pip install -e .[doc]
+ Write-Output "The package has successfully been installed in the virtual environment." | Green
+ Write-Output "The documentation is about to be built." | Green
 .\doc\make.bat html
+ Write-Output "The documentation has been successfully built." | Green
 .\doc\_build\html\index.html
+deactivate
+cd ..
