28787e89396b7a890d0fffd57502ea8132d40c00
Camille <78221213+clatapie@users.noreply.github.com>
2024-12-12 17:56:11 +0100

    feat: add ``project_name`` configuration

--- src/pyconverter/xml2py/writer.py
+++ src/pyconverter/xml2py/writer.py
@@ -220,6 +220,7 @@ def write_global__init__file(library_path: Path, config_path: Path) -> None:
         Path object of the directory containing the generated package.
     """
 
+    project_name = get_config_data_value(config_path, "project_name")
     subfolder_values = get_config_data_value(config_path, "subfolders")
 
     if subfolder_values:
@@ -244,7 +245,7 @@ def write_global__init__file(library_path: Path, config_path: Path) -> None:
         fid.write("except ModuleNotFoundError:\n")
         fid.write("    import importlib_metadata\n\n")
         fid.write("__version__ = importlib_metadata.version(__name__.replace('.', '-'))\n")
-        fid.write('"""PyConverter-GeneratedCommands version."""\n')
+        fid.write(f'"""{project_name} version."""\n')
     fid.close()
 
 
