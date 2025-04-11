5a90cc7fdb46c8691ff412a7d7d09ca071d44cc6
Camille <78221213+clatapie@users.noreply.github.com>
2025-04-11 11:43:08 +0200

    fix: url rendering

--- src/pyconverter/xml2py/ast_tree.py
+++ src/pyconverter/xml2py/ast_tree.py
@@ -3392,13 +3392,14 @@ class XMLCommand(Element):
         """Python-formatted notes string."""
         lines = [section_title, "-" * len(section_title)]
         if section_title == "Notes" and self._is_paragraph_in_arg_desc:
-            lines.append("")
-            lines.append(".. warning::")
-            lines.append(
-                "   This function contains specificities regarding the argument definitions."
-            )
-            lines.append(f"   Please refer to the `Ansys documentation <{self.url}`_")
-            lines.append("   for further explanations.")
+            warning_message = [
+                "",
+                ".. warning::",
+                "   This function contains specificities regarding the argument definitions.",
+                "   Please refer to the `command documentation <{self.url}>`_",
+                "   for further explanations.",
+            ]
+            lines.extend(warning_message)
         for note in note_elem_list:
             if note.title and str(note.title).strip() != section_title:
                 note_title = str(note.title).strip()
